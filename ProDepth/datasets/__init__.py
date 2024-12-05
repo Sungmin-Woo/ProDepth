f670a54947ca44bab309db79179c1e164cbfb8bf
LeeWonJoon9868 <92299426+LeeWonJoon9868@users.noreply.github.com>
2024-12-05 16:41:02 +0900

    Delete ProDepth directory

--- ProDepth/datasets/__init__.py
+++
@@ -1,8 +0,0 @@
-# flake8: noqa: F401
-from .kitti_dataset import KITTIRAWDataset, KITTIOdomDataset, KITTIDepthDataset
-from .cityscapes_preprocessed_dataset import CityscapesPreprocessedDataset
-from .cityscapes_evaldataset import CityscapesEvalDataset
-#from .cityscapes_evaldataset_dynamic import CityscapesEvalDataset
-#from .waymo_dataset import WaymoDataset
-#from .nusc_dataset import NuscDataset
-#from .nuscenes_dataset import nuScenesDataset
--- ProDepth/networks/__init__.py
+++
@@ -1,6 +0,0 @@
-from .resnet_encoder import ResnetEncoder, ResnetEncoderMatching
-from .depth_decoder import DepthDecoder
-from .depth_decoder_uncert import DepthDecoderUncert
-from .pose_decoder import PoseDecoder
-from .pose_cnn import PoseCNN
-from .depth_encoder import LiteMono
